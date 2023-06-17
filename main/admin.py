aefa6b46214a608e40f49ae69b49411c2299ea74
Alexandr321321 <windamania@mail.ru>
2023-06-17 17:25:57 +0300

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'nickname', 'email', 'usertype')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('label', 'id', 'username', 'price', 'category')
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('username', 'product_id')
+
+class SellsAdmin(admin.ModelAdmin):
+    list_display = ('username', 'product_id', 'buyer', 'label', 'description', 'price', 'auction', 'category')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Basket, BasketAdmin)
+admin.site.register(Sells, SellsAdmin)
